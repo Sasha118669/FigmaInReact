325dc240bc900b113c94a590c90003ad0439334b
Sasha118669 <sashashukshyn@gmail.com>
2025-12-01 18:58:43 +0100

    Update from new device

--- src/assest.js
+++ src/assest.js
@@ -1,82 +1,84 @@
 // Иконки
-import HeartIcon from 'C:/for job/figma in react/figma-in-react/src/assets/icons/HeartIcon.jsx';
-import SearchIcon from 'C:/for job/figma in react/figma-in-react/src/assets/icons/SearchIcon.jsx';
-import AccountIcon from 'C:/for job/figma in react/figma-in-react/src/assets/icons/AccountIcon.jsx';
-import CartIcon from 'C:/for job/figma in react/figma-in-react/src/assets/icons/CartIcon.jsx';
-import ArrowIcon from 'C:/for job/figma in react/figma-in-react/src/assets/icons/ArrowIcon.jsx';
-import Arrow2Icon from 'C:/for job/figma in react/figma-in-react/src/assets/icons/Arrow2Icon.jsx';
-import ShareIcon from 'C:/for job/figma in react/figma-in-react/src/assets/icons/ShareIcon.jsx';
-import CompareIcon from 'C:/for job/figma in react/figma-in-react/src/assets/icons/CompareIcon.jsx';
-import TrophyIcon from 'C:/for job/figma in react/figma-in-react/src/assets/icons/TrophyIcon.jsx';
-import CheckMarkIcon from 'C:/for job/figma in react/figma-in-react/src/assets/icons/CheckMarkIcon.jsx';
-import BoxIcon from 'C:/for job/figma in react/figma-in-react/src/assets/icons/BoxIcon.jsx';
-import SupportIcon from 'C:/for job/figma in react/figma-in-react/src/assets/icons/SupportIcon.jsx';
-import FilterIcon from 'C:/for job/figma in react/figma-in-react/src/assets/icons/FilterIcon.jsx';
-import GridIcon from 'C:/for job/figma in react/figma-in-react/src/assets/icons/GridIcon.jsx';
-import AlignJustifyIcon from 'C:/for job/figma in react/figma-in-react/src/assets/icons/AlignJustifyIcon.jsx';
-import LocationIcon from 'C:/for job/figma in react/figma-in-react/src/assets/icons/LocationIcon.jsx';
-import PhoneIcon from 'C:/for job/figma in react/figma-in-react/src/assets/icons/PhoneIcon.jsx';
-import TimeIcon from 'C:/for job/figma in react/figma-in-react/src/assets/icons/TimeIcon.jsx';
-import DeleteFilledIcon from 'C:/for job/figma in react/figma-in-react/src/assets/icons/DeleteFilledIcon.jsx';
-import DeleteIcon from 'C:/for job/figma in react/figma-in-react/src/assets/icons/DeleteIcon.jsx';
-import BagDeleteIcon from 'C:/for job/figma in react/figma-in-react/src/assets/icons/BagDeleteIcon.jsx';
-import AdminIcon from 'C:/for job/figma in react/figma-in-react/src/assets/icons/AdminIcon.jsx';
-import CalendarIcon from 'C:/for job/figma in react/figma-in-react/src/assets/icons/CalendarIcon.jsx';
-import TagIcon from 'C:/for job/figma in react/figma-in-react/src/assets/icons/TagIcon.jsx';
-import FacebookIcon from 'C:/for job/figma in react/figma-in-react/src/assets/icons/FacebookIcon.jsx';
-import TwitterIcon from 'C:/for job/figma in react/figma-in-react/src/assets/icons/TwitterIcon.jsx';
-import LinkedinIcon from 'C:/for job/figma in react/figma-in-react/src/assets/icons/LinkedinIcon.jsx';
+import HeartIcon from '@assets/icons/HeartIcon.jsx'
+import SearchIcon from '@assets/icons/SearchIcon.jsx'
+import AccountIcon from '@assets/icons/AccountIcon.jsx'
+import CartIcon from '@assets/icons/CartIcon.jsx'
+import ArrowIcon from '@assets/icons/ArrowIcon.jsx'
+import Arrow2Icon from '@assets/icons/Arrow2Icon.jsx'
+import ShareIcon from '@assets/icons/ShareIcon.jsx'
+import CompareIcon from '@assets/icons/CompareIcon.jsx'
+import TrophyIcon from '@assets/icons/TrophyIcon.jsx'
+import CheckMarkIcon from '@assets/icons/CheckMarkIcon.jsx'
+import BoxIcon from '@assets/icons/BoxIcon.jsx'
+import SupportIcon from '@assets/icons/SupportIcon.jsx'
+import FilterIcon from '@assets/icons/FilterIcon.jsx'
+import GridIcon from '@assets/icons/GridIcon.jsx'
+import AlignJustifyIcon from '@assets/icons/AlignJustifyIcon.jsx'
+import LocationIcon from '@assets/icons/LocationIcon.jsx'
+import PhoneIcon from '@assets/icons/PhoneIcon.jsx'
+import TimeIcon from '@assets/icons/TimeIcon.jsx'
+import DeleteFilledIcon from '@assets/icons/DeleteFilledIcon.jsx'
+import DeleteIcon from '@assets/icons/DeleteIcon.jsx'
+import BagDeleteIcon from '@assets/icons/BagDeleteIcon.jsx'
+import AdminIcon from '@assets/icons/AdminIcon.jsx'
+import CalendarIcon from '@assets/icons/CalendarIcon.jsx'
+import TagIcon from '@assets/icons/TagIcon.jsx'
+import FacebookIcon from '@assets/icons/FacebookIcon.jsx'
+import TwitterIcon from '@assets/icons/TwitterIcon.jsx'
+import LinkedinIcon from '@assets/icons/LinkedinIcon.jsx'
+
 // SVG (если используете как путь)
-import HouseLogoIcon from 'C:/for job/figma in react/figma-in-react/src/assets/icons/HouseLogoIcon.svg';
+import HouseLogoIcon from '@assets/icons/HouseLogoIcon.svg'
+
 // Фотографии
-import DiningImg from 'C:/for job/figma in react/figma-in-react/src/assets/pictures/First-img-SecondSectionMain.png';
-import LivingImg from 'C:/for job/figma in react/figma-in-react/src/assets/pictures/Second-img-SecondSectionMain.png';
-import BedroomImg from 'C:/for job/figma in react/figma-in-react/src/assets/pictures/Third-img-SecondSectionMain.png';
+import DiningImg from '@assets/pictures/First-img-SecondSectionMain.png'
+import LivingImg from '@assets/pictures/Second-img-SecondSectionMain.png'
+import BedroomImg from '@assets/pictures/Third-img-SecondSectionMain.png'
 
-import Images1 from 'C:/for job/figma in react/figma-in-react/src/assets/pictures/Images1.png';
-import Images2 from 'C:/for job/figma in react/figma-in-react/src/assets/pictures/Images2.png';
-import Images3 from 'C:/for job/figma in react/figma-in-react/src/assets/pictures/Images3.png';
-import Images4 from 'C:/for job/figma in react/figma-in-react/src/assets/pictures/Images4.png';
-import Images5 from 'C:/for job/figma in react/figma-in-react/src/assets/pictures/Images5.png';
-import Images6 from 'C:/for job/figma in react/figma-in-react/src/assets/pictures/Images6.png';
-import Images7 from 'C:/for job/figma in react/figma-in-react/src/assets/pictures/Images7.png';
-import Images8 from 'C:/for job/figma in react/figma-in-react/src/assets/pictures/Images8.png';
+import Images1 from '@assets/pictures/Images1.png'
+import Images2 from '@assets/pictures/Images2.png'
+import Images3 from '@assets/pictures/Images3.png'
+import Images4 from '@assets/pictures/Images4.png'
+import Images5 from '@assets/pictures/Images5.png'
+import Images6 from '@assets/pictures/Images6.png'
+import Images7 from '@assets/pictures/Images7.png'
+import Images8 from '@assets/pictures/Images8.png'
 
-import LeftSideImg1 from 'C:/for job/figma in react/figma-in-react/src/assets/pictures/LeftSideImg1.png';
-import LeftSideImg2 from 'C:/for job/figma in react/figma-in-react/src/assets/pictures/LeftSideImg2.png';
-import LeftSideImg3 from 'C:/for job/figma in react/figma-in-react/src/assets/pictures/LeftSideImg3.png';
-import LeftSideImg4 from 'C:/for job/figma in react/figma-in-react/src/assets/pictures/LeftSideImg4.png';
+import LeftSideImg1 from '@assets/pictures/LeftSideImg1.png'
+import LeftSideImg2 from '@assets/pictures/LeftSideImg2.png'
+import LeftSideImg3 from '@assets/pictures/LeftSideImg3.png'
+import LeftSideImg4 from '@assets/pictures/LeftSideImg4.png'
 
-import MidleImg from 'C:/for job/figma in react/figma-in-react/src/assets/pictures/MidleImg.png';
+import MidleImg from '@assets/pictures/MidleImg.png'
 
-import RightSideImg1 from 'C:/for job/figma in react/figma-in-react/src/assets/pictures/RightSideImg1.png';
-import RightSideImg2 from 'C:/for job/figma in react/figma-in-react/src/assets/pictures/RightSideImg2.png';
-import RightSideImg3 from 'C:/for job/figma in react/figma-in-react/src/assets/pictures/RightSideImg3.png';
-import RightSideImg4 from 'C:/for job/figma in react/figma-in-react/src/assets/pictures/RightSideImg4.png';
+import RightSideImg1 from '@assets/pictures/RightSideImg1.png'
+import RightSideImg2 from '@assets/pictures/RightSideImg2.png'
+import RightSideImg3 from '@assets/pictures/RightSideImg3.png'
+import RightSideImg4 from '@assets/pictures/RightSideImg4.png'
 
-import AsgaardSofa5 from 'C:/for job/figma in react/figma-in-react/src/assets/pictures/AsgaardSofa5.png';
+import AsgaardSofa5 from '@assets/pictures/AsgaardSofa5.png'
 
-import ShopingCartBoxImg1 from 'C:/for job/figma in react/figma-in-react/src/assets/pictures/ShopingCartBoxImg1.png';
+import ShopingCartBoxImg1 from '@assets/pictures/ShopingCartBoxImg1.png'
 
-import BlogContentImg1 from 'C:/for job/figma in react/figma-in-react/src/assets/pictures/BlogContentImg1.png';
-import BlogContentImg2 from 'C:/for job/figma in react/figma-in-react/src/assets/pictures/BlogContentImg2.png';
-import BlogContentImg3 from 'C:/for job/figma in react/figma-in-react/src/assets/pictures/BlogContentImg3.png';
+import BlogContentImg1 from '@assets/pictures/BlogContentImg1.png'
+import BlogContentImg2 from '@assets/pictures/BlogContentImg2.png'
+import BlogContentImg3 from '@assets/pictures/BlogContentImg3.png'
 
-import RightSideBlogRecentPostImg1 from 'C:/for job/figma in react/figma-in-react/src/assets/pictures/RightSideBlogRecentPostImg1.png';
-import RightSideBlogRecentPostImg2 from 'C:/for job/figma in react/figma-in-react/src/assets/pictures/RightSideBlogRecentPostImg2.png';
-import RightSideBlogRecentPostImg3 from 'C:/for job/figma in react/figma-in-react/src/assets/pictures/RightSideBlogRecentPostImg3.png';
-import RightSideBlogRecentPostImg4 from 'C:/for job/figma in react/figma-in-react/src/assets/pictures/RightSideBlogRecentPostImg4.png';
-import RightSideBlogRecentPostImg5 from 'C:/for job/figma in react/figma-in-react/src/assets/pictures/RightSideBlogRecentPostImg5.png';
+import RightSideBlogRecentPostImg1 from '@assets/pictures/RightSideBlogRecentPostImg1.png'
+import RightSideBlogRecentPostImg2 from '@assets/pictures/RightSideBlogRecentPostImg2.png'
+import RightSideBlogRecentPostImg3 from '@assets/pictures/RightSideBlogRecentPostImg3.png'
+import RightSideBlogRecentPostImg4 from '@assets/pictures/RightSideBlogRecentPostImg4.png'
+import RightSideBlogRecentPostImg5 from '@assets/pictures/RightSideBlogRecentPostImg5.png'
 
-import ProductComarisonTableImg from 'C:/for job/figma in react/figma-in-react/src/assets/pictures/ProductComarisonTableImg.png';
+import ProductComarisonTableImg from '@assets/pictures/ProductComarisonTableImg.png'
 
-import SingleProductImg from 'C:/for job/figma in react/figma-in-react/src/assets/pictures/SingleProductImg.png';
-import SingleProductImg1 from 'C:/for job/figma in react/figma-in-react/src/assets/pictures/SingleProductImg1.png';
-import SingleProductImg2 from 'C:/for job/figma in react/figma-in-react/src/assets/pictures/SingleProductImg2.png';
-import SingleProductImg3 from'C:/for job/figma in react/figma-in-react/src/assets/pictures/SingleProductImg3.png';
-import SingleProductImg4 from 'C:/for job/figma in react/figma-in-react/src/assets/pictures/SingleProductImg4.png';
+import SingleProductImg from '@assets/pictures/SingleProductImg.png'
+import SingleProductImg1 from '@assets/pictures/SingleProductImg1.png'
+import SingleProductImg2 from '@assets/pictures/SingleProductImg2.png'
+import SingleProductImg3 from '@assets/pictures/SingleProductImg3.png'
+import SingleProductImg4 from '@assets/pictures/SingleProductImg4.png'
 
-import DescriptionImg from 'C:/for job/figma in react/figma-in-react/src/assets/pictures/DescriptionImg.png';
+import DescriptionImg from '@assets/pictures/DescriptionImg.png'
 
 // Экспорт всех ассетов
 export {
